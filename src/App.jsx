e78f1a64b978bf5cd8743ec050d087d69cb78b23
Süfî Yan Raza <123558804+Sufi-Yan-Raza@users.noreply.github.com>
2023-12-13 21:02:41 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,9 @@ import { HiOutlineLightBulb } from "react-icons/hi";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { MdOutlineAvTimer } from "react-icons/md";
 import { FaRegCheckCircle } from "react-icons/fa";
-
+import { FaCalendarDays } from "react-icons/fa6";
+import { TiDeleteOutline } from "react-icons/ti";
+import { IoMdInformationCircleOutline } from "react-icons/io";
 
 import qorrectLogo from "./assets/images/qorrect-logo.png";
 import bellIcon from "./assets/images/bell.png";
@@ -39,7 +41,7 @@ function App() {
               type="text"
               placeholder="Search Courses, Items, Exams......."
             />
-            {<CiSearch className="search-icon" />}
+            <CiSearch className="search-icon" />
           </div>
           <div className="nav-right-side">
             <img src={bellIcon} alt="Bell Icon" className="bell-icon" />
@@ -64,6 +66,7 @@ function App() {
           <div className="side-menu-items ">
             <FaSitemap className="icon" />
             <p>Items</p>
+            <span>12</span>
           </div>
           <div className="side-menu-items">
             <LuFileSpreadsheet className="icon" />
@@ -148,42 +151,157 @@ function App() {
               </div>
               <div className="students-data">
                 <div className="cards">
-                <div className="boxes">
-                  <LuUser2 className="icon" />
-                  <div className="details">
-                    <p>Total Students</p>
-                    <p className="number">400</p>
+                  <div className="boxes">
+                    <LuUser2 className="icon" />
+                    <div className="details">
+                      <p>Total Students</p>
+                      <p className="number">400</p>
+                    </div>
+                  </div>
+                  <div className="boxes b-2">
+                    <HiOutlineLightBulb className="icon" />
+                    <div className="details">
+                      <p>Total Active Students</p>
+                      <p className="number">278</p>
+                    </div>
+                  </div>
+                  <div className="boxes b-3">
+                    <HiOutlineExclamationCircle className="icon" />
+                    <div className="details">
+                      <p>Total Absent Students</p>
+                      <p className="number">23</p>
+                    </div>
+                  </div>
+                  <div className="boxes b-4">
+                    <MdOutlineAvTimer className="icon" />
+                    <div className="details">
+                      <p>Total Pending Students</p>
+                      <p className="number"></p>
+                    </div>
+                  </div>
+                  <div className="boxes b-5">
+                    <FaRegCheckCircle className="icon" />
+                    <div className="details">
+                      <p>Total Finished Students</p>
+                      <p className="number">278</p>
+                    </div>
                   </div>
                 </div>
-                <div className="boxes b-2">
-                  <HiOutlineLightBulb  className="icon" />
-                  <div className="details">
-                    <p>Total Active Students</p>
-                    <p className="number">278</p>
-                  </div>
-                </div>
-                <div className="boxes b-3">
-                  <HiOutlineExclamationCircle className="icon" />
-                  <div className="details">
-                    <p>Total Absent Students</p>
-                    <p className="number">23</p>
-                  </div>
-                </div>
-                <div className="boxes b-4">
-                  <MdOutlineAvTimer className="icon" />
-                  <div className="details">
-                    <p>Total Pending Students</p>
-                    <p className="number"></p>
-                  </div>
-                </div>
-                <div className="boxes b-5">
-                  <FaRegCheckCircle className="icon" />
-                  <div className="details">
-                    <p>Total Finished Students</p>
-                    <p className="number">278</p>
-                  </div>
-                </div>
-                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="students-database">
+            <div className="db-heading">
+              <p className="students">Students</p>
+              <span className="student-list-heading">Showing 7 Students</span>
+            </div>
+
+            <div className="db-filters">
+              <div className="input-search">
+                <input type="text" placeholder="Search name or e-mail" />
+                <CiSearch className="search-icon icon" />
+              </div>
+
+              <div className="status">
+                <p>Status : all</p>
+                <FaChevronDown className="icon" />
+              </div>
+
+              <div className="status">
+                <p>Start Time</p>
+                <FaCalendarDays className="icon" />
+              </div>
+
+              <div className="status">
+                <p>Submit Date</p>
+                <FaCalendarDays className="icon" />
+              </div>
+
+              <div className="clear ">
+                <TiDeleteOutline className="icon" />
+                <p>clear</p>
+              </div>
+            </div>
+
+            <div className="active-filters">
+              <div className="name-filter filter">
+                <p>Student name</p>
+                <FaChevronDown className="icon " />
+              </div>
+
+              <div className="email-filter filter">
+                <p>Email</p>
+                <FaChevronDown className="icon" />
+              </div>
+
+              <div className="status-filter filter">
+                <p>Status</p>
+                <FaChevronDown className="icon" />
+              </div>
+
+              <div className="start-date-filter filter">
+                <p>Start Date</p>
+                <FaChevronDown className="icon " />
+              </div>
+
+              <div className="start-time-filter filter">
+                <p>Start time</p>
+                <FaChevronDown className="icon" />
+              </div>
+            </div>
+
+            <div className="student-field">
+              <div className="name-field">
+                <img src={userIcon} />
+                <p>Cannon Matusov</p>
+              </div>
+
+              <div className="email-field">
+                <p>happin_rabbti@gmail.com</p>
+              </div>
+
+              <div className="status-field">
+                <p>Finished</p>
+                <IoMdInformationCircleOutline
+                  title="I am happy"
+                  className="icon"
+                />
+              </div>
+
+              <div className="start-time-field">
+                <p>09 Nov 2019, 9:00 AM</p>
+              </div>
+
+              <div className="submit-time-field">
+                <p>23 July 2020, 12:20 AM</p>
+              </div>
+            </div>
+
+            <div className="student-field sf2">
+              <div className="name-field">
+                <img src={userIcon} />
+                <p>Cannon Matusov</p>
+              </div>
+
+              <div className="email-field">
+                <p>happin_rabbti@gmail.com</p>
+              </div>
+
+              <div className="status-field">
+                <p>Finished</p>
+                <IoMdInformationCircleOutline
+                  title="I am happy"
+                  className="icon"
+                />
+              </div>
+
+              <div className="start-time-field">
+                <p>09 Nov 2019, 9:00 AM</p>
+              </div>
+
+              <div className="submit-time-field">
+                <p>23 July 2020, 12:20 AM</p>
               </div>
             </div>
           </div>
